ff1269277ae8686c6f8e60623584339a1b631540
Gelei Chen <chen1474@purdue.edu>
2016-04-18 23:36:58 -0400

    add use rid to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419033357) do
+ActiveRecord::Schema.define(version: 20160419033639) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160419033357) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
