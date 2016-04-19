e5156a6902fa1ee887abc76f7687fe5098d6f3ad
Gelei Chen <chen1474@purdue.edu>
2016-04-18 23:39:41 -0400

    add show & search method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,14 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   # GET /movies
   # GET /movies.json
+  def search
+    if params[:search].present?
+      @movies = Movie.search(params[:search])
+    else
+      @movies = Movie.all
+    end
+  end
+
   def index
     @movies = Movie.all
   end
@@ -10,6 +18,13 @@ class MoviesController < ApplicationController
   # GET /movies/1
   # GET /movies/1.json
   def show
+    @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
   # GET /movies/new
