ce1a988d9b78433c5f0dacd66fcf7a3427059bce
Grey <chen1474@purdue.edu>
2016-04-21 20:26:09 -0400

    update views&background

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   #needed for devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
